117f870460f21620737ee557a01f12d71df80411
dsd95 <diogo.duarte.t0127648@edu.atec.pt>
2026-03-17 17:05:03 +0000

    Delete transform.py

--- transform.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.getOrCreate()
-
-df = spark.table("dab_demo_bronze.data")
-df = df.withColumnRenamed("id", "value")
-
-df.write.mode("overwrite").saveAsTable("dab_demo_silver.data_clean")
