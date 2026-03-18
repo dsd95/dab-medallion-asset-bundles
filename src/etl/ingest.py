047645916b14b307d82b82970bf28156d36a1889
dsd95 <diogo.duarte.t0127648@edu.atec.pt>
2026-03-18 12:39:56 +0000

    Change DataFrame source and rename column

--- src/etl/ingest.py
+++ src/etl/ingest.py
@@ -2,5 +2,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
 
-df = spark.range(1000)
+df = spark.table("dab_demo_bronze.data")
+df = df.withColumnRenamed("id", "value")
+
 df.write.mode("overwrite").saveAsTable("medallion_lab.dab_demo_bronze.data")
